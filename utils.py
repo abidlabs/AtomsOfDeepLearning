ab57f7f5b1a60345452ccd916813b1de495134ac
Abubakar Abid <aabid@DESKTOP-ROLE13U.stanford.edu>
2018-01-02 02:09:00 -0600

    Experiment 3

--- utils.py
+++ utils.py
@@ -1,6 +1,43 @@
 import tensorflow as tf
 import numpy as np
-import time
+import time    
+from IPython.display import clear_output, Image, display, HTML
+
+def strip_consts(graph_def, max_const_size=32):
+    """Strip large constant values from graph_def."""
+    strip_def = tf.GraphDef()
+    for n0 in graph_def.node:
+        n = strip_def.node.add() 
+        n.MergeFrom(n0)
+        if n.op == 'Const':
+            tensor = n.attr['value'].tensor
+            size = len(tensor.tensor_content)
+            if size > max_const_size:
+                tensor.tensor_content = "<stripped %d bytes>"%size
+    return strip_def
+
+def show_graph(graph_def, max_const_size=32):
+    """Visualize TensorFlow graph."""
+    if hasattr(graph_def, 'as_graph_def'):
+        graph_def = graph_def.as_graph_def()
+    strip_def = strip_consts(graph_def, max_const_size=max_const_size)
+    code = """
+        <script>
+          function load() {{
+            document.getElementById("{id}").pbtxt = {data};
+          }}
+        </script>
+        <link rel="import" href="https://tensorboard.appspot.com/tf-graph-basic.build.html" onload=load()>
+        <div style="height:600px">
+          <tf-graph-basic id="{id}"></tf-graph-basic>
+        </div>
+    """.format(data=repr(str(strip_def)), id='graph'+str(np.random.rand()))
+
+    iframe = """
+        <iframe seamless style="width:1200px;height:620px;border:0" srcdoc="{}"></iframe>
+    """.format(code.replace('"', '&quot;'))
+    display(HTML(iframe))
+
 
 class Timer():
     def __init__(self):
@@ -12,9 +49,92 @@ class Timer():
     def end_and_print(self):
         print("Elapsed time:",np.round(time.time()-self.time,3),"s")
 
-        
-### COMMON FUNCTIONS ###
 
+##
+
+import matplotlib.pyplot as plt
+
+def draw_neural_net(ax, left, right, bottom, top, layer_sizes):
+    '''
+    Credit: https://gist.github.com/craffel/2d727968c3aaebd10359
+    Draw a neural network cartoon using matplotilb.
+    
+    :usage:
+        >>> fig = plt.figure(figsize=(12, 12))
+        >>> draw_neural_net(fig.gca(), .1, .9, .1, .9, [4, 7, 2])
+    
+    :parameters:
+        - ax : matplotlib.axes.AxesSubplot
+            The axes on which to plot the cartoon (get e.g. by plt.gca())
+        - left : float
+            The center of the leftmost node(s) will be placed here
+        - right : float
+            The center of the rightmost node(s) will be placed here
+        - bottom : float
+            The center of the bottommost node(s) will be placed here
+        - top : float
+            The center of the topmost node(s) will be placed here
+        - layer_sizes : list of int
+            List of layer sizes, including input and output dimensionality
+    '''
+    
+    n_layers = len(layer_sizes)
+    v_spacing = (top - bottom)/float(max(layer_sizes))
+    h_spacing = (right - left)/float(len(layer_sizes) - 1)
+    # Nodes
+    for n, layer_size in enumerate(layer_sizes):
+        layer_top = v_spacing*(layer_size - 1)/2. + (top + bottom)/2.
+        for m in range(layer_size):
+            circle = plt.Circle((n*h_spacing + left, layer_top - m*v_spacing), v_spacing/4.,
+                                color='w', ec='k', zorder=4)
+            ax.add_artist(circle)
+    # Edges
+    for n, (layer_size_a, layer_size_b) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
+        layer_top_a = v_spacing*(layer_size_a - 1)/2. + (top + bottom)/2.
+        layer_top_b = v_spacing*(layer_size_b - 1)/2. + (top + bottom)/2.
+        for m in range(layer_size_a):
+            for o in range(layer_size_b):
+                line = plt.Line2D([n*h_spacing + left, (n + 1)*h_spacing + left],
+                                  [layer_top_a - m*v_spacing, layer_top_b - o*v_spacing], c='k')
+                ax.add_artist(line)
+
+
+##
+        
+### COMMON ANALYTICAL FUNCTIONS ###
+
+def random_batch(x_values, y_values,size=64):
+    assert x_values.shape[0]==y_values.shape[0]
+    n = x_values.shape[0]
+    indices = np.random.permutation(n)[:size]
+    return x_values[indices], y_values[indices]
+
+def random_values():
+    def random_functions(x_values):
+        n, d = x_values.shape
+        return np.random.normal(0,1,n)
+    return random_functions
+    
+
+def sigmoid(x): 
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_of_sigmoid():
+    def sigmoid_of_sigmoid_function(x_values):
+        y_values = sigmoid(sigmoid(x_values[:,0]+x_values[:,1])+sigmoid(x_values[:,2]+x_values[:,3]))
+        return y_values
+    return sigmoid_of_sigmoid_function
+
+def polynomial_composition(power=2):
+    def polynomial_composition_function(x_values):
+        n, d = x_values.shape
+        x_values = np.add.reduceat(x_values, axis=1, indices=range(0,d,2)) #adds adjacent columns together
+        x_values = x_values**power
+        n, d = x_values.shape
+        x_values = np.add.reduceat(x_values, axis=1, indices=range(0,d,2)) #adds adjacent columns together
+        x_values = x_values**power
+        return np.sum(x_values,axis=1)
+    return polynomial_composition_function
 
 def polynomial_to_power(power=2):
     from scipy.misc import factorial
@@ -32,6 +152,11 @@ def polynomial(coefs=[1,1,1]):
         return np.polynomial.polynomial.polyval(x_values,coefs)
     return polynomial_function
 
+def sparse_trig():
+    def sparse_trig_function(x_values):
+        return 2*(2*np.cos(x_values)**2-1)**2-1
+    return sparse_trig_function
+
 ### END COMMON FUNCTIONS ###
 
 '''
@@ -42,24 +167,65 @@ num_input: number of nodes in input layer
 num_output: number of nodes in output layer
 activation: the tensorflow activation function to user
 '''
-def multilayer_perceptron(x, num_nodes, num_input=1, num_output=1, activation=tf.nn.sigmoid):
+def multilayer_perceptron(x, num_nodes, num_input=1, num_output=1, activation=tf.nn.sigmoid, bias=True):
     n_prev = num_input
     out = x
+    num_layer = 0
     for n in num_nodes:
-        w = tf.Variable(tf.random_normal([n_prev, n]))
-        b = tf.Variable(tf.random_normal([n]))
-        
-        out = activation(tf.add(tf.matmul(out,w),b))
+        w = tf.get_variable("w"+str(num_layer),[n_prev, n])        
+        if bias:
+            b = tf.get_variable("b"+str(num_layer),[n])
+            out = activation(tf.add(tf.matmul(out,w),b),name="out"+str(num_layer))
+        else:
+            out = activation(tf.matmul(out,w),name="out"+str(num_layer))
+            
         n_prev = n
+        num_layer += 1
         
-    w_out = tf.Variable(tf.random_normal([n, num_output]))
-    b_out = tf.Variable(tf.random_normal([num_output]))
-    out = tf.add(tf.matmul(out,w_out),b_out)
+    w_out = tf.get_variable("w"+str(num_layer),[n, num_output])
+    if bias:
+        b_out = tf.get_variable("b"+str(num_layer),[num_output])
+        out = tf.add(tf.matmul(out,w_out),b_out,name="out"+str(num_layer))
+    else:
+        out = tf.matmul(out,w_out,name="out"+str(num_layer))
+        
     return out
+
+
+# Modified MLP for use with experiment 2
+def recurrent_multilayer_perceptron(x, num_nodes, num_input=1, num_output=1, activation=tf.nn.sigmoid):
+    n_prev = num_input
+    
+    assert all(x == num_nodes[0] for x in num_nodes) #for a recurrent multilayer perceptron, the number of neurons in each hidden layer should be the same
+    
+    w_in = tf.get_variable("w_in",[n_prev, num_nodes[0]])
+    b_in = tf.get_variable("b_in",[num_nodes[0]])
+    
+    w = tf.get_variable("w_shared",[num_nodes[0], num_nodes[0]])
+    b = tf.get_variable("b_shared",[num_nodes[0]])
+    
+    for i in range(len(num_nodes)+1):
+        if i==0:
+            out = activation(tf.add(tf.matmul(x,w_in),b_in),name="out"+str(i))
+        else:
+            out = activation(tf.add(tf.matmul(out,w),b),name="out"+str(i))
+        
+    w_out = tf.get_variable("w_out",[num_nodes[0], num_output])
+    b_out = tf.get_variable("b_out",[num_output])
+    out = tf.add(tf.matmul(out,w_out),b_out,name="out_final")
+    
+    return out
+
+
+#################
+# All of the Experiments, starting from a base class
+#################
 
 class Experiment():
     def __init__(self):
         pass
+    
+    #ideally, this would make experiments completely reproducible, but because jobs are distributed over multiple cores, small differences may persist in practice
     def initialize(self):
         np.random.seed(0)
         tf.set_random_seed(0)
@@ -74,7 +240,7 @@ class Experiment1(Experiment):
         
     def run(self,
             n_hidden = 2,
-            learning_rate = 0.01,
+            learning_rate = 0.003,
             num_steps = 10000,
             num_values = 100,
             function = sin(omega=6),
@@ -113,26 +279,34 @@ class Experiment2(Experiment):
         pass
         
     def run(self,
-            d = 4,
-            n=1000, 
-            n_hidden=[8],
-            num_steps=10000,
-            learning_rate = 0.01,
-            verbose=True,
-            function=polynomial_to_power(power=4)):
+            n=16, 
+            n_hidden=[10],
+            num_steps=15000,
+            learning_rate = 0.003,
+            verbose=False,
+            recurrent=True):
         
         
-        x_values = np.random.normal(0,1,(n,d))
-        y_values = function(x_values).reshape(-1,1)
-
+        x_values = np.linspace(0,1-1/n,n).reshape(-1,1)
+        y_values = np.resize([[0,1],[1,0]], (n,2))
+        
         tf.reset_default_graph()
-        x = tf.placeholder(dtype="float", shape=[None,d])
-        y = tf.placeholder(dtype="float", shape=[None,1])
-        losses = []
+        x = tf.placeholder(dtype="float", shape=[None,1])
+        y = tf.placeholder(dtype="float", shape=[None,2])
 
-        y_ = multilayer_perceptron(x, num_input=d, num_nodes=n_hidden)
+        if recurrent:
+            y_ = recurrent_multilayer_perceptron(x, num_input=1, num_output=2, num_nodes=n_hidden,activation=tf.nn.relu)
+        else:
+            y_ = multilayer_perceptron(x, num_input=1, num_output=2, num_nodes=n_hidden,bias=bias,activation=tf.nn.relu)
 
-        loss_op = tf.reduce_mean(tf.square(y_ - y))
+        
+        n_params = np.sum([np.product([xi.value for xi in x.get_shape()]) for x in tf.global_variables()])        
+        #show_graph(tf.get_default_graph().as_graph_def())
+        loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=y_))
+        correct_prediction = tf.equal(tf.argmax(y_, 1), tf.argmax(y, 1))
+        accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        
+        #loss_op = tf.reduce_mean(tf.square(y_ - y))
         optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
         train_op = optimizer.minimize(loss_op)
         init_op = tf.global_variables_initializer()
@@ -140,6 +314,64 @@ class Experiment2(Experiment):
         with tf.Session() as sess:
             sess.run(init_op)
             for step in range(num_steps):
-                _, loss, y_pred = sess.run([train_op, loss_op, y_], feed_dict={x:x_values,y:y_values})
+                x_batch, y_batch = random_batch(x_values, y_values)
+                _, loss, y_pred = sess.run([train_op, loss_op, y_], feed_dict={x:x_batch,y:y_batch})
+                if (step%(num_steps/10)==0 and verbose):
+                    print(loss)
+        
+            accuracy, loss, y_pred = sess.run([accuracy_op, loss_op, y_], feed_dict={x:x_values,y:y_values})
 
-        return loss
+        return x_values.squeeze(), y_values.squeeze(), y_pred.squeeze(), loss, accuracy, n_params
+
+'''
+Experiment 3: With How Much Data Are Neural Networks Better than Other Techniques?
+'''
+class Experiment3(Experiment):
+    def __init__(self):
+        pass
+        
+    def run(self,
+            classifiers, 
+            d = 12,
+            d_extra = 0,
+            class_sep = 1,
+            ns = np.logspace(2,4,10),
+            iters = 3,
+            covariance_scale = 1,
+            test_size = 0.2,
+            accuracy_on = 'test',
+            recurrent=True):
+        
+        import warnings
+        from sklearn.model_selection import train_test_split
+        from sklearn.datasets import make_classification
+        
+        acc = np.zeros((len(ns),len(classifiers)))
+        n_max = int(np.max(ns))
+
+        c = covariance_scale*np.random.random((d+d_extra,d+d_extra))
+        cov = c.T.dot(c)
+        
+        X1 = np.random.multivariate_normal([0]*d+[0]*d_extra, cov, size=int(n_max/2))
+        X2 = np.random.multivariate_normal([class_sep]*d+[0]*d_extra, cov, size=int(n_max/2))
+        
+        X = np.concatenate([X1,X2])
+        y = np.array([0]*int(n_max/2) + [1]*int(n_max/2))
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+
+        for i, n in enumerate(ns):
+            for j, clf in enumerate(classifiers):
+                for _ in range(iters):
+                    with warnings.catch_warnings():
+                        warnings.simplefilter('ignore')
+                        clf.fit(X_train[:int(n*0.8)],y_train[:int(n*0.8)])
+                        if accuracy_on=='train':
+                            acc[i,j] += clf.score(X_train[:int(n*0.8)],y_train[:int(n*0.8)])
+                        elif accuracy_on=='test':
+                            acc[i,j] += clf.score(X_test,y_test)
+                        else:
+                            raise ValueError("accuracy_on=='test' or accuracy_on=='train'") #the accuracy must be measured on the training or test set
+
+        return acc/iters
+
+
